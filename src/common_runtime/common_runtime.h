a2063e446d65410d9541645595d2c6d2b6336249
Zhenyu Guo <guozhen.gzy@alipay.com>
2018-03-16 12:25:00 +0800

    init directory structure for common runtime

---
+++ src/common_runtime/common_runtime.h
@@ -0,0 +1,32 @@
+/**
+ * This file defines the API interface for a common local runtime
+ * that is useful for building most distributed systems.
+ * 
+ * Goal of this interface:
+ *   - meet the common requirements for building most distributed systems
+ *   - general enough for possibility of plugging in new implmenetations (e.g., providers)
+ *   - the generalization should not introduce runtime overhead
+ *      * performance wise, we should use macros for that
+ *      * but, simple forwarded call should be able to be inlined by compilers, so here
+ *        we still use function wrappers instead
+ */
+# pragma once
+
+// logging
+# include "interface/logging.h"
+
+// configuration
+# include "interface/configuration.h"
+
+// performance counters
+# include "interface/perf_counter.h"
+
+// rpc 
+// TODO: we can use grpc as the standard interface
+# include "interface/rpc.h"
+
+// event loop (including timers)
+# include "interface/event_looper.h"
+
+// TODO: disk io
+
